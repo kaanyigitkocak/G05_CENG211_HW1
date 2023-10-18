6d0a144f0ae4628ebb6fe472cbde831338e6e6c5
dkwns <kaandogan2013@gmail.com>
2023-10-18 20:07:18 +0300

    asdads

--- src/G05_CENG211_HW1/SalesManagement.java
+++ src/G05_CENG211_HW1/SalesManagement.java
@@ -1,10 +1,10 @@
 package G05_CENG211_HW1;
 
 public class SalesManagement {
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println();
 	}
 
 }
